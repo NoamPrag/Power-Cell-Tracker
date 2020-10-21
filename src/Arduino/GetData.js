d2455cfb0bb7c8f1f22e9bfc27a33222938fdafd
NoamPrag <noam.prag07@gmail.com>
2020-10-21 21:50:41 +0300

    Changed export in GetData to modern JS export

--- src/Arduino/GetData.js
+++ src/Arduino/GetData.js
@@ -2,9 +2,4 @@ const fs = require("fs");
 const path = require("path");
 
 const rawData = fs.readFileSync(path.resolve("./src/Arduino/Coords.json"));
-const parsed = JSON.parse(rawData);
-console.log(parsed.bursts);
-
-module.exports = {
-  data: parsed.bursts,
-};
+export const parsed = JSON.parse(rawData);
