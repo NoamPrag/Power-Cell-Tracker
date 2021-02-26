9ae59f5fccea7e74832fa02df02105f00c1cbb1f
NoamPrag <noam.prag07@gmail.com>
2021-02-27 00:49:15 +0200

    Bursts slide from right with animation.

--- src/Burst.tsx
+++ src/Burst.tsx
@@ -5,6 +5,7 @@ import {
   AccordionDetails,
   Container,
   Typography,
+  Slide,
 } from "@material-ui/core";
 import { ExpandMore } from "@material-ui/icons";
 import SportsSoccerIcon from "@material-ui/icons/SportsSoccer";
@@ -40,88 +41,90 @@ const tabTheme = createMuiTheme({
 const Burst = (props: {
   burst: BurstData;
   color: string;
-  changeOpen?: () => void;
+  changeOpen: () => void;
 }): JSX.Element => {
   const [opened, setOpened] = useState(false);
 
   return (
-    <Container maxWidth="sm" style={{ margin: "10px 0" }}>
-      <Accordion>
-        <ThemeProvider theme={tabTheme}>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            onClick={() => {
-              setOpened((current: boolean): boolean => !current);
-              props.changeOpen();
-            }}
-          >
-            <Typography
-              variant="h5"
-              style={{ color: props.color, transitionDuration: "0.7s" }}
+    <Slide direction="left" in mountOnEnter unmountOnExit>
+      <Container maxWidth="sm" style={{ margin: "10px 0" }}>
+        <Accordion>
+          <ThemeProvider theme={tabTheme}>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              onClick={() => {
+                setOpened((current: boolean): boolean => !current);
+                props.changeOpen();
+              }}
             >
-              Burst #{props.burst.burstNumber}
-            </Typography>
-          </AccordionSummary>
-        </ThemeProvider>
-        <AccordionDetails
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div
+              <Typography
+                variant="h5"
+                style={{ color: props.color, transitionDuration: "0.7s" }}
+              >
+                Burst #{props.burst.burstNumber}
+              </Typography>
+            </AccordionSummary>
+          </ThemeProvider>
+          <AccordionDetails
             style={{
-              width: "100%",
               display: "flex",
-              flexDirection: "column",
               alignItems: "center",
+              justifyContent: "center",
             }}
           >
             <div
               style={{
+                width: "100%",
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: "column",
                 alignItems: "center",
-                width: "95%",
-                padding: "0 10px",
               }}
             >
-              <GpsFixedIcon />
-              <ProgressBar value={props.burst.accuracy} show={opened} />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "95%",
+                  padding: "0 10px",
+                }}
+              >
+                <GpsFixedIcon />
+                <ProgressBar value={props.burst.accuracy} show={opened} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "95%",
+                  padding: "0 10px",
+                }}
+              >
+                <GrainIcon />
+                <ProgressBar value={props.burst.precision} show={opened} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                {props.burst.inInnerPort.map(
+                  (inInnerPort: boolean, index: number): JSX.Element => (
+                    <SportsSoccerIcon
+                      key={index}
+                      fontSize="large"
+                      color={inInnerPort ? "secondary" : "primary"}
+                    />
+                  )
+                )}
+              </div>
             </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                width: "95%",
-                padding: "0 10px",
-              }}
-            >
-              <GrainIcon />
-              <ProgressBar value={props.burst.precision} show={opened} />
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-              }}
-            >
-              {props.burst.inInnerPort.map(
-                (inInnerPort: boolean, index: number): JSX.Element => (
-                  <SportsSoccerIcon
-                    key={index}
-                    fontSize="large"
-                    color={inInnerPort ? "secondary" : "primary"}
-                  />
-                )
-              )}
-            </div>
-          </div>
-        </AccordionDetails>
-      </Accordion>
-    </Container>
+          </AccordionDetails>
+        </Accordion>
+      </Container>
+    </Slide>
   );
 };
 
