9dc220a7e9b8e9b8eb54c9b516ed3578ff4afcf8
NoamPrag <noam.prag07@gmail.com>
2021-03-04 14:33:22 +0200

    The new power cells are shown as big grey dots

--- src/ScatterChart.tsx
+++ src/ScatterChart.tsx
@@ -13,15 +13,22 @@ interface ScatterProps {
 }
 
 const ScatterChart = (props: ScatterProps): JSX.Element => {
-  const datasets: {}[] = props.data.map(
-    (burst: BurstData, index: number): {} => ({
+  const datasets: {}[] = [
+    ...props.data.map((burst: BurstData, index: number): {} => ({
       pointRadius: 10,
       hoverRadius: 14,
       label: `Burst #${burst.burstNumber}`,
       data: burst.burstCoordinates,
       backgroundColor: props.colors[index],
-    })
-  );
+    })),
+    {
+      data: props.newBurst,
+      backgroundColor: "#e5e5e5", // grey color
+      label: `Burst #${props.data.length + 1}`,
+      pointRadius: 20,
+      hoverRadius: 28,
+    },
+  ];
   return <Scatter data={{ datasets }} height={400} options={options} />;
 };
 
